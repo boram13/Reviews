df93497c5b41962d54b4f718b5c0667701c78660
boram13 <bmenerja@helius-systems.com>
2024-04-01 16:42:06 +0200

    Email verification and add redis

--- service/cache.js
+++ service/cache.js
@@ -16,39 +16,13 @@ class Cache {
 
     async set(key, value) {
         return await this.client.set(key, value)
-        // the following is used in case of ioredis package
-        // if (!this.client) {
-        //     throw new Error('Client is not connected!');
-        // }
-        // return new Promise((resolve, reject) => {
-        //     this.client.set(key, value, (err, reply) => {
-        //         if (err) {
-        //             console.error(err);
-        //             reject(err);
-        //         } else {
-        //             resolve(reply);
-        //         }
-        //     });
-        // });
+        
     }
 
 
     async get(key) {
         return await this.client.get(key)
-        // // the following is used in case of ioredis package
-        // if (!this.client) {
-        //     throw new Error('Client is not connected!');
-        // }
-        // return new Promise((resolve, reject) => {
-        //     this.client.get(key, (err, reply) => {
-        //         if (err) {
-        //             console.error('problem in geting value', err);
-        //             reject(err);
-        //         } else {
-        //             resolve(reply);
-        //         }
-        //     })
-        // })
+     
     }
 }
 
